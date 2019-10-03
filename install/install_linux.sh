8a61470dda4ddb0fa7549b52962259e6f0d9f398
robbin bouwmeester <robbin.bouwmeester@ugent.be>
2019-10-03 10:01:24 +0200

    Actually install the correct environment...

--- install/install_linux.sh
+++ install/install_linux.sh
@@ -1,4 +1,4 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x ./Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh -b -p ./Miniconda3
-Miniconda3/condabin/conda env create -f environment.yml
+Miniconda3/condabin/conda env create -f environment_linux.yml
