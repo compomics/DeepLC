de4e2e280fb12585b7524d0f7a030c9c231f328d
RobbinBouwmeester <Robbin.bouwmeester@ugent.be>
2023-05-08 15:07:47 +0200

    fix dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='deeplc',
-    version='2.1.1',
+    version='2.1.2',
     license='apache-2.0',
     description='DeepLC: Retention time prediction for (modified) peptides using Deep Learning.',
     long_description=LONG_DESCRIPTION,
@@ -50,7 +50,8 @@ setup(
         'pygam>=0.8.0,<1',
         'scikit-learn>=0.24.0,<2',
         'deeplcretrainer>=0.1,<1',
-        'psm_utils>=0.2.3,<1'
+        'psm_utils>=0.2.3,<1',
+        'hdf5plugin>=4.1.1'
     ],
     extras_require={
         "gui": ["gooey>=1.0"],
